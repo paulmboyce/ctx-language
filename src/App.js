e44118d8bef033fcac3adea17874eb0c4fcda324
paulmboyce <paul.boyce@goobito.com>
2021-02-10 12:50:58 +0000

    feature(baseline): initial version of app function
    no ux, remove default create-react-app artifacts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [lblName, setLblName] = useState("Name");
+	const [lblSubmit, setLblSubmit] = useState("OK");
+	return (
+		<div>
+			Select a language:
+			<span>US</span>
+			<span
+				onClick={() => {
+					setLblName("Nome");
+					setLblSubmit("Esta Bem");
+				}}
+			>
+				Portuguese
+			</span>
+			<label htmlFor="id-name">{lblName}</label>
+			<input id="id-name" type="text"></input>
+			<div role="button">{lblSubmit}</div>
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,63 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test <App>", function () {
+	beforeEach(function () {
+		//ARR
+		render(<App />);
+	});
+
+	it("renders select a language", () => {
+		const linkElement = screen.getByText(/SELECT A LANGUAGE/i);
+		expect(linkElement).toBeInTheDocument();
+	});
+
+	it("renders US option", function () {
+		const el = screen.getByText(/US/i);
+		expect(el).toBeInTheDocument();
+	});
+
+	it("renders PORTUGUESE option", function () {
+		//ASS
+		const el = screen.getByText(/PORTUGUESE/i);
+		expect(el).toBeInTheDocument();
+	});
+
+	it("renders a submit button", function () {
+		//ASS
+		const btn = screen.getByRole("button", { name: /OK/i });
+		expect(btn).toBeInTheDocument();
+	});
+
+	it("has an NAME input field", function () {
+		//ASS
+		const input = screen.getByLabelText(/NAME/i);
+		expect(input).toBeInTheDocument();
+	});
+
+	it("shows label, button in PORTUGUESE, when click on PT language", async function () {
+		//ACT
+		const selectPT = screen.getByText(/PORTUGUESE/i);
+		fireEvent.click(selectPT);
+
+		//ASS
+		await waitFor(function () {
+			const translated = screen.getByLabelText(/NOME/i);
+			expect(translated).toBeInTheDocument();
+			const btn = screen.getByRole("button", { name: /ESTA BEM/i });
+			expect(btn).toBeInTheDocument();
+		});
+	});
+
+	it("shows label in ENGLISH, when click on US language", async function () {
+		//ACT
+		const selectPT = screen.getByText(/US/i);
+		fireEvent.click(selectPT);
+
+		//ASS
+		await waitFor(function () {
+			const translated = screen.getByLabelText(/NAME/i);
+			expect(translated).toBeInTheDocument();
+		});
+	});
 });
